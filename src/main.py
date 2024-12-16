ee28a6dd950dc73f2673b1707728065d590bd1af
Patryk Gacek <haniv1001@gmail.com>
2024-12-17 00:46:25 +0100

    fixed bfs alg. and improved graph drawing

--- src/main.py
+++ src/main.py
@@ -1,37 +1,30 @@
 from models.bfs_community_graph import BfsCommunityGraph
-import networkx as nx
 
-# 1. Stworzenie obiektu klasy BfsCommunityGraph
-community_graph = BfsCommunityGraph()
 
-# 2. Stworzenie grafu z listy krawędzi
-edges = [
+people = ["Patryk", "Kasia", "Kajetan", "Ania", "Artur", "Karolina", "Tomek", "Klaudia", "Krzysiek", "Asia"]
+
+connections = [
+    ("Patryk", "Kasia"),
     ("Patryk", "Kajetan"),
-    ("Kajetan", "Artur"),
-    ("Patryk", "Julia"),
-    ("Julia", "Anna"),
-    ("Anna", "Zosia"),
-    ("Zosia", "Kajetan"),
- ]
-community_graph.make_graph_from(edges)
+    ("Patryk", "Ania"),
+    ("Patryk", "Artur"),
+    ("Kasia", "Karolina"),
+    ("Kasia", "Tomek"),
+    ("Kajetan", "Klaudia"),
+    ("Kajetan", "Krzysiek"),
+    ("Ania", "Asia"),
+    ("Ania", "Krzysiek"),
+    ("Artur", "Karolina"),
+    ("Artur", "Tomek"),
+    ("Karolina", "Klaudia"),
+    ("Klaudia", "Krzysiek"),
+    ("Krzysiek", "Asia")
+]
 
-# 3. Ustawienie węzła startowego (np. "Patryk") i uruchomienie algorytmu BFS
-start_node = "Patryk"
-if community_graph.set_start_node(start_node):
-    print(f"Algorytm BFS rozpoczęty od węzła: {start_node}")
-else:
-    print("Podany węzeł startowy nie istnieje w grafie.")
-
-# 4. Wyświetlenie wyników BFS
-print("Wynik BFS (rozprzestrzenianie się informacji):")
-print("Kolejność rozprzestrzeniania:", community_graph.spread_order)
-print("Poziomy (dystans) do startowego węzła:", community_graph.levels)
-
-# 5. Narysowanie grafu rozprzestrzeniania się informacji
-community_graph.draw()
-
-graph = BfsCommunityGraph()
-graph.random_community(10, 0.2)
-graph.set_start_node(0)
-
-print(graph)
+spread_graph = BfsCommunityGraph(debug=True)
+spread_graph.make_graph(people, connections)
+# spread_graph.random_community(20, 0.2)
+order = spread_graph.run()
+# spread_graph.draw_connections()
+spread_graph.draw_compare()
+# spread_graph.draw_spread()
